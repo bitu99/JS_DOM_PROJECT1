19598b71a3f43aaeccf00e43e59ee5a314ba7db9
AKASH PANDEY <106162919+bitu99@users.noreply.github.com>
2023-09-12 15:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ button.onclick = function(){
                             button.innerHTML="remove";
                             button.style.backgroundColor = "white";
                             button.style.color = "black";
+                        
                             h2.innerHTML = "Friend";
                             h2.style.color = "green";
                         };
