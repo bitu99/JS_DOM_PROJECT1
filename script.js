b94fa5cef5c1163f21109889bee6a4fb1121e386
AKASH PANDEY <106162919+bitu99@users.noreply.github.com>
2023-09-12 10:00:00 +0000

    changes in javascript for infinite button fuctionality and not just one time

--- script.js
+++ script.js
@@ -1,11 +1,111 @@
 var button = document.querySelector('#btn');
 var h2 = document.querySelector('h2');
+var a=0;
 
 button.onclick = function(){
-    button.innerHTML="request sent";
+    button.innerHTML="remove";
     button.style.backgroundColor = "white";
+    button.style.color = "black";
 
     h2.innerHTML = "Friend";
     h2.style.color = "green";
 
+    button.onclick = function(){
+        h2.innerHTML = "Stranger";
+        h2.style.color = "red";
+            
+        button.innerHTML="Make friend";
+        button.style.backgroundColor = "cornflowerblue";
+        button.style.color = "white";
+
+        button.onclick = function(){
+            button.innerHTML="remove";
+            button.style.backgroundColor = "white";
+            button.style.color = "black";
+        
+            h2.innerHTML = "Friend";
+            h2.style.color = "green";
+
+            button.onclick = function(){
+                h2.innerHTML = "Stranger";
+                h2.style.color = "red";
+                    
+                button.innerHTML="Make friend";
+                button.style.backgroundColor = "cornflowerblue";
+                button.style.color = "white";
+
+                button.onclick = function(){
+                    button.innerHTML="remove";
+                    button.style.backgroundColor = "white";
+                    button.style.color = "black";
+                
+                    h2.innerHTML = "Friend";
+                    h2.style.color = "green";
+
+                    button.onclick = function(){
+                        h2.innerHTML = "Stranger";
+                        h2.style.color = "red";
+                            
+                        button.innerHTML="Make friend";
+                        button.style.backgroundColor = "cornflowerblue";
+                        button.style.color = "white";
+
+                        button.onclick = function(){
+                            button.innerHTML="remove";
+                            button.style.backgroundColor = "white";
+                            button.style.color = "black";
+                        
+                            h2.innerHTML = "Friend";
+                            h2.style.color = "green";
+                        };
+                    };
+                };
+            };
+        };
+    };
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
